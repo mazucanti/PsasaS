5d84635b53fcfd6b29ffc3883fbffecff185d85f
mazucanti <daniel.mazucanti.ismart@gmail.com>
2020-03-15 18:45:31 -0300

    importação da função sleep em todos os módulos do programa

--- Trabalho.py
+++ Trabalho.py
@@ -1,11 +1,3 @@
-import Etapas.Construcao
-#OU
-from Etapas import Construcao
-# e dá pra definir um alias
-import Etapas.Implementacao as imp
+from time import sleep
 
 
-
-def nome_func(parametro, parametro2):
-    #Linha de comentário
-    #não precisa definir os tipos dos parâmetros
