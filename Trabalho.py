adfc0ba31d8c39bf219f5fe3c4a18bd2bd34c1bd
mazucanti <daniel.mazucanti.ismart@gmail.com>
2020-03-15 18:38:57 -0300

    sintaxe básica

--- Trabalho.py
+++ Trabalho.py
@@ -1,3 +1,11 @@
-while(True):
-    print("jaaj")
-    
+import Etapas.Construcao
+#OU
+from Etapas import Construcao
+# e dá pra definir um alias
+import Etapas.Implementacao as imp
+
+
+
+def nome_func(parametro, parametro2):
+    #Linha de comentário
+    #não precisa definir os tipos dos parâmetros
